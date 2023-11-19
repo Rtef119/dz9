06babbfb67e0d25187f4dea3041f7a5b9e0d6359
rtef119 <rtef119@gmail.com>
2023-11-19 12:48:31 +0100

    dz9

--- main.py
+++ main.py
@@ -6,5 +6,7 @@ import requests
 resource = requests.get("https://bank.gov.ua/")
 if resource.status_code==200:
     soup = BeautifulSoup(resource.text,features="html.parser")
-    Dolar = soup.find("p","<div class='col-xs-4'><div class='left'><div class='value index-page'>39<small>,3054</small>")
+    Dolar = soup.find("p",'<span class="DFlfde SwHCTb" data-precision="2" data-value="35.942400000000006">35,94</span>')
     print(Dolar.text)
+    Evro = soup.find("p","<span class='DFlfde SwHCTb' data-precision='2' data-value='39.2401152'>39,24</span>")
+    print(Evro.text)
